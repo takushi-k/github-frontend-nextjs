04f274bf2cb7feb04734cfc412a65d0f95346284
takushi-k <50552608+takushi-k@users.noreply.github.com>
2024-09-12 19:55:05 +0900

    z-2 (#33)

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Function E-1 Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Z-2 Function E-1 Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
